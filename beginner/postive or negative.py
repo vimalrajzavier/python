7ea170bc6cf9beeafae906aee2c18bb5f7609b10
vimalrajzavier <37236195+vimalrajzavier@users.noreply.github.com>
2018-03-10 15:16:27 +0530

    Create postive or negative.py

---
+++ beginner/postive or negative.py
@@ -0,0 +1,12 @@
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+a=int(input("enter the integer"))
+if(a==0):
+   print("zero")
+elif(a>0):
+    print("positive value")
+else:
+    print("negative")
